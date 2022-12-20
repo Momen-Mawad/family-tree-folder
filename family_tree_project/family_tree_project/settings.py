a5a34c1e74a0da74887b383c4511caa391053d9e
Momen <eng.momen3@gmail.com>
2022-12-20 10:58:34 +0100

    workflow

--- family_tree_project/family_tree_project/settings.py
+++ family_tree_project/family_tree_project/settings.py
@@ -37,7 +37,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
     'http://localhost:8000',
-    'http://ec2-34-228-19-244.compute-1.amazonaws.com'
+    'http://ec2-44-208-33-172.compute-1.amazonaws.com'
 ]
 
 # Application definition
