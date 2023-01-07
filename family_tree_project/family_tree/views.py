e97ec811aab7f638a36c7261ef90700ee63c7cab
Momen <eng.momen3@gmail.com>
2023-01-07 23:48:31 +0100

    sorted names

--- family_tree_project/family_tree/views.py
+++ family_tree_project/family_tree/views.py
@@ -1,6 +1,6 @@
 from rest_framework import viewsets
 from .models import Person, Family
-from .serializers import FamilySerializer, PersonSerializer, DPersonSerializer
+from .serializers import FamilySerializer, PersonSerializer
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import permissions
